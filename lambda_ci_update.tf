14464999bd640ac2127ba1ac5c3927d07c97a6f3
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2024-06-21 13:02:19 -0400

    fix referenc

--- lambda_ci_update.tf
+++ lambda_ci_update.tf
@@ -33,7 +33,7 @@ resource "aws_api_gateway_resource" "playground_ci" {
 
 resource "aws_api_gateway_method" "playground_ci" {
   rest_api_id   = aws_api_gateway_rest_api.playground.id
-  resource_id   = aws_api_gateway_resource.playground.id
+  resource_id   = aws_api_gateway_resource.playground_ci.id
   http_method   = "POST"
   authorization = "NONE"
 
