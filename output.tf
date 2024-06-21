45c8c12b85f6f8e20a6b2f7c11f681378b328986
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2024-06-21 12:48:45 -0400

    add the bucket name as an output

--- output.tf
+++ output.tf
@@ -7,6 +7,10 @@ output "playground_lambda_invoke_arn" {
   value = aws_lambda_function.playground.invoke_arn
 }
 
+output "bucket_name" {
+  value = module.placeholder_bucket.name.s3_bucket_id
+}
+
 # output "hcp_webhook_resource_name" {
 #   description = "API resource name of the HCP notification webhook."
 #   value       = hcp_notifications_webhook.aws.resource_name
