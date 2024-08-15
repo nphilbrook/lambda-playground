5a091c4bf9d2b49a04bd50ff953fda24fd766ae8
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2024-08-15 13:51:04 -0400

    role, policy, attachment for GHA

--- gh_oidc.tf
+++ gh_oidc.tf
@@ -7,3 +7,67 @@ resource "aws_iam_openid_connect_provider" "gha_provider" {
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [data.tls_certificate.gha_certificate.certificates[0].sha1_fingerprint]
 }
+
+# Creates a role which can only be used by the specified GHA
+#
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role
+resource "aws_iam_role" "gha_role" {
+  name = "gha-role"
+
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Effect": "Allow",
+     "Principal": {
+       "Federated": "${aws_iam_openid_connect_provider.gha_provider.arn}"
+     },
+     "Action": "sts:AssumeRoleWithWebIdentity",
+     "Condition": {
+       "StringEquals": {
+         "token.actions.githubusercontent.com:aud": "${one(aws_iam_openid_connect_provider.gha_provider.client_id_list)}"
+       },
+       "StringLike": {
+         "token.actions.githubusercontent.com:sub": "repo:nphilbrook/lambda-fib:ref:refs/heads/*"
+       }
+     }
+   }
+ ]
+}
+EOF
+}
+
+# Creates a policy that will be used to define the permissions that
+# the previously created role has within AWS.
+#
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy
+resource "aws_iam_policy" "gha_policy" {
+  name        = "gha-policy"
+  description = "GHA run policy to update S3 and Lambdao"
+
+  policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Effect": "Allow",
+     "Action": [
+       "lambda:UpdateFunctionCode",
+       "s3:PutObject"
+     ],
+     "Resource": "*"
+   }
+ ]
+}
+EOF
+}
+
+# Creates an attachment to associate the above policy with the
+# previously created role.
+#
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment
+resource "aws_iam_role_policy_attachment" "gha_policy_attachment" {
+  role       = aws_iam_role.gha_role.name
+  policy_arn = aws_iam_policy.gha_policy.arn
+}
