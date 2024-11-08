d76bd740a964fa5a06c32947efac3255072d4389
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2024-11-08 05:50:47 -0500

    pin module to this version

--- module.tf
+++ module.tf
@@ -1,4 +1,4 @@
 module "function" {
-  source = "git@github.com:nphilbrook/terraform-aws-function-with-api.git"
+  source   = "git@github.com:nphilbrook/terraform-aws-function-with-api.git?ref=6a2482e"
   basename = "function-2"
 }
