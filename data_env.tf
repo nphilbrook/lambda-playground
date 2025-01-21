30d6711eb6703fb6b036efd8d8e190d9cce184fb
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-01-21 11:40:01 -0500

    and the modules

--- data_env.tf
+++ data_env.tf
@@ -2,7 +2,13 @@ module "terraform_data_env" {
   source           = "github.com/abuxton/terraform-data-env"
 }
 
-output "terraform_module_versions" {
+output "env_variables" {
   value       = module.terraform_data_env.environment_data_all
+  description = "The versions of the modules used in this configuration and all environment variables"
+}
+
+
+output "terraform_module_versions" {
+  value       = module.terraform_data_env.modules_json
   description = "The versions of the modules used in this configuration and all environment variables"
 }
