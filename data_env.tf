874a39e2123f730ae3d65a1e21bcf5b27d224a90
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-01-31 08:49:18 -0500

    update output descriptions

--- data_env.tf
+++ data_env.tf
@@ -4,11 +4,11 @@ module "terraform_data_env" {
 
 output "env_variables" {
   value       = module.terraform_data_env.environment_data_all
-  description = "The versions of the modules used in this configuration and all environment variables"
+  description = "All environment variables."
 }
 
 
 output "terraform_module_versions" {
   value       = module.terraform_data_env.modules_json
-  description = "The versions of the modules used in this configuration and all environment variables"
+  description = "The versions of the modules used in this configuration."
 }
