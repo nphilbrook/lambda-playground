f594fc15aadaaf37d8f0a0df267b4ff0a9648a10
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2026-01-12 13:22:24 -0500

    Add IAM to the other one

--- lambda_ci_update.tf
+++ lambda_ci_update.tf
@@ -35,7 +35,7 @@ resource "aws_api_gateway_method" "playground_ci" {
   rest_api_id   = aws_api_gateway_rest_api.playground.id
   resource_id   = aws_api_gateway_resource.playground_ci.id
   http_method   = "POST"
-  authorization = "NONE"
+  authorization = "AWS_IAM"
 
   request_parameters = {
     "method.request.header.X-Foo-Signature" = false
